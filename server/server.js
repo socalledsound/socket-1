ec8718ad5655cb04c3388ea5daf643da2b4cfd12
Douglas Fleischut <df@douglass-imac.lan>
2017-10-21 20:16:51 -0700

    currently not working

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
+
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
+var server = http.createServer(app);
+var io = socketIO.listen(server);
 
 app.use(express.static(publicPath));
 
