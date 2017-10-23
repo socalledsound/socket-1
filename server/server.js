5ed4b0d56498f4b5d82cb94879b49a3636f9b9fb
Douglas Fleischut <df@douglass-imac.lan>
2017-10-23 13:17:59 -0700

    through lecture 107

--- server/server.js
+++ server/server.js
@@ -9,10 +9,21 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
-var io = socketIO.listen(server);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=> {
+io.on('connection', (socket)=> {
+    console.log('new user connected');
+
+    socket.on('disconnect', ()=>{
+        console.log('message');
+    })
+
+})
+
+
+
+server.listen(port, ()=> {
     console.log(`server running on ${port}`);
 })
